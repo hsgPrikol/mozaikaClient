e39686377163701c36be8888c13d63b4bc6a5c1e
Tihon <Tihon>
2022-04-11 17:56:11 +0300

    очинь ВАЖНЫЙ COMMENT

--- Client/super_server/userdialog.h
+++ Client/super_server/userdialog.h
@@ -18,7 +18,7 @@ private:
     QString avatarPath;
     QVector<QString> members;
     QVector<Message> messages;
-public:
+public:// очень важный комментарий
 
     UserDialog(int _dialog_id, QString _name="", QByteArray _avatar = nullptr, QString _avatarPath=""): dialog_id(_dialog_id), name(_name), avatar(_avatar), avatarPath(_avatarPath) {}
     UserDialog(QString _name="", QByteArray _avatar = nullptr): UserDialog(-1, _name, _avatar) {}
